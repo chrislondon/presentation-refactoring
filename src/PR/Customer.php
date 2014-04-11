f5fdc0a39f02d480e3e47fa34f781ca2b21ab7e2
Chris London <me@chrislondon.co>
2014-04-11 15:37:48 -0600

    extracting get charge from customer statement function

--- src/PR/Customer.php
+++ src/PR/Customer.php
@@ -50,27 +50,6 @@ class Customer {
 		$result = "Rental Record for " . $this->getName() . "\n";
 
 		foreach ($rentals as $each) {
-			$thisAmount = 0;
-
-			//determine amounts for each line
-			switch ($each->getMovie()->getPriceCode()) {
-				case Movie::REGULAR:
-					$thisAmount += 2;
-
-					if ($each->getDaysRented() > 2)
-						$thisAmount += ($each->getDaysRented() - 2) * 1.5;
-					break;
-
-				case Movie::NEW_RELEASE:
-					$thisAmount += $each->getDaysRented() * 3;
-					break;
-				case Movie::CHILDRENS:
-					$thisAmount += 1.5;
-					if ($each->getDaysRented() > 3)
-						$thisAmount += ($each->getDaysRented() - 3) * 1.5;
-					break;
-			}
-
 			// add frequent renter points
 			$frequentRenterPoints++;
 
@@ -80,13 +59,39 @@ class Customer {
 
 			//show figures for this rental
 
-			$result .= "\t" . $each->getMovie()->getTitle() . "\t" . $thisAmount . "\n";
+			$result .= "\t" . $each->getMovie()->getTitle() . "\t" . $this->getCharge($each) . "\n";
 
-			$totalAmount += $thisAmount;
+			$totalAmount += $this->getCharge($each);
 		}
 
 		$result .= 'Amount owed is ' . $totalAmount . "\n";
 		$result .= 'You earned ' . $frequentRenterPoints . " frequent renter points\n";
+
+		return $result;
+	}
+
+	public function getCharge(Rental $rental)
+	{
+		$result = 0;
+
+		//determine amounts for each line
+		switch ($rental->getMovie()->getPriceCode()) {
+			case Movie::REGULAR:
+				$result += 2;
+
+				if ($rental->getDaysRented() > 2)
+					$result += ($rental->getDaysRented() - 2) * 1.5;
+				break;
+
+			case Movie::NEW_RELEASE:
+				$result += $rental->getDaysRented() * 3;
+				break;
+			case Movie::CHILDRENS:
+				$result += 1.5;
+				if ($rental->getDaysRented() > 3)
+					$result += ($rental->getDaysRented() - 3) * 1.5;
+				break;
+		}
 
 		return $result;
 	}
